f9b91fc3e02256f162e3cb338b5d849e127d099e
dharmil <dddshinde3@gmail.com>
2024-08-03 18:45:00 +0530

    updated

--- src/app/cube/page.tsx
+++ src/app/cube/page.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import style from './page.module.css'
+import React from "react";
+import style from "./page.module.css";
 const page = () => {
   return (
     <div className={style.container}>
-        <div className={style.cube}>
-            <div className={style.front}></div> 
-            <div className={style.back}></div>
-            <div className={style.top}></div>
-            <div className={style.bottom}></div>
-            <div className={style.left}></div>
-            <div className={style.right}></div>
-        </div>
-        </div>
+      <div className={style.cube}>
+        <div className={style.front}></div>
+        <div className={style.back}></div>
+        <div className={style.top}></div>
+        <div className={style.bottom}></div>
+        <div className={style.left}></div>
+        <div className={style.right}></div>
+      </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default page
+export default page;
